0b5e617c10065e067663b36182185e9a6d3e33ea
rla523at <rla523at@pearlabyss.com>
2024-08-16 16:48:38 +0900

    개선 완료 및 이해할 수 없는 결과

--- Project1/Project1/hlsl/predict_density_error_and_update_pressure_CS.hlsl
+++ Project1/Project1/hlsl/predict_density_error_and_update_pressure_CS.hlsl
@@ -18,8 +18,6 @@ StructuredBuffer<uint>                  ncount_buffer           : register(t3);
 
 RWStructuredBuffer<float> density_buffer        : register(u0);
 RWStructuredBuffer<float> pressure_buffer       : register(u1);
-RWStructuredBuffer<float> density_error_buffer  : register(u2);
-
 
 [numthreads(NUM_THREAD,1,1)]
 void main(uint3 DTid : SV_DispatchThreadID)
@@ -49,9 +47,87 @@ void main(uint3 DTid : SV_DispatchThreadID)
   rho *= g_m0;
   
   const float denisty_error = rho - g_rho0;
+  const float p = pressure_buffer[fp_index];
 
-  density_buffer[fp_index]        = rho;
-  density_error_buffer[fp_index]  = denisty_error;  
-  pressure_buffer[fp_index]       +=  scailing_factor_buffer[0] * denisty_error;  
-  pressure_buffer[fp_index]       = max(0.0, pressure_buffer[fp_index]);  
+  density_buffer[fp_index]  = rho;
+  pressure_buffer[fp_index] = max(0.0, p + scailing_factor_buffer[0] * denisty_error);  
 }
+
+
+//#define NUM_THREAD 128
+//#define NUM_MAX_GROUP 65535
+//#define N 2
+//
+//#include "cubic_spline_kernel.hlsli"
+//#include "uniform_grid_output.hlsli"
+//
+//cbuffer CB : register(b1)
+//{
+//  float g_rho0;
+//  float g_m0;
+//  uint  g_num_fp;
+//  uint  g_estimated_num_NFP;
+//};
+//
+//StructuredBuffer<float3>                pos_buffer              : register(t0);
+//StructuredBuffer<float>                 scailing_factor_buffer  : register(t1);
+//StructuredBuffer<Neighbor_Information>  ninfo_buffer            : register(t2);
+//StructuredBuffer<uint>                  ncount_buffer           : register(t3);
+//
+//RWStructuredBuffer<float> density_buffer        : register(u0);
+//RWStructuredBuffer<float> pressure_buffer       : register(u1);
+//
+//groupshared float shared_sum_W[NUM_THREAD];
+//
+//[numthreads(NUM_THREAD,1,1)]
+//void main(uint3 Gid : SV_GroupID, uint Gindex : SV_GroupIndex)
+//{
+//  const uint fp_index = Gid.x + Gid.y*NUM_MAX_GROUP; 
+//
+//  if (g_num_fp <= fp_index)
+//    return;
+//
+//  const uint num_nfp = ncount_buffer[fp_index];
+//
+//  const float3 v_xi = pos_buffer[fp_index];
+//
+//  float sum_W = 0.0;
+//
+//  for (uint i=0; i<N; ++i)
+//  {
+//    const uint index = Gindex * N + i;
+//
+//    if (num_nfp <= index)
+//        break;
+//
+//    const uint ninfo_index  = fp_index * g_estimated_num_NFP + index;
+//    const uint nbr_fp_index = ninfo_buffer[ninfo_index].fp_index;
+//
+//    const float3  v_xj      = pos_buffer[nbr_fp_index];
+//    const float   distance  = length(v_xi-v_xj);
+//
+//    sum_W += W(distance);
+//  }
+//
+//  shared_sum_W[Gindex] = sum_W;
+//
+//  GroupMemoryBarrierWithGroupSync(); 
+//
+//  if (Gindex == 0)
+//  {
+//    float rho = 0.0;
+//    
+//    const uint n = min(NUM_THREAD, num_nfp);
+//  
+//    for (uint i=0; i <n; ++i)
+//      rho += shared_sum_W[i];
+//
+//    rho *= g_m0;
+//
+//    const float denisty_error = rho - g_rho0;
+//    const float p             = pressure_buffer[fp_index];
+//
+//    density_buffer[fp_index]  = rho;
+//    pressure_buffer[fp_index] = max(0.0, p + scailing_factor_buffer[0] * denisty_error);  
+//  }
+//}
