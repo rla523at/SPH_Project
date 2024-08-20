b5db5e2b9742610c043809dd29f8c4539630e664
rla523at <rla523at@pearlabyss.com>
2024-08-20 16:20:18 +0900

    V33

--- Project1/Project1/hlsl/predict_density_error_and_update_pressure_CS.hlsl
+++ Project1/Project1/hlsl/predict_density_error_and_update_pressure_CS.hlsl
@@ -1,7 +1,7 @@
-#define NUM_THREAD 256
+#define NUM_THREAD 64
 #define NUM_MAX_GROUP 65535
-#define N 2
 #define NUM_FP_CHUNK_MAX 64
+#define N 4
 
 #include "cubic_spline_kernel.hlsli"
 #include "uniform_grid_output.hlsli"
@@ -30,7 +30,7 @@ StructuredBuffer<Local_Nbr_Sum>         local_nbr_sum_buffer    : register(t6);
 RWStructuredBuffer<float> density_buffer        : register(u0);
 RWStructuredBuffer<float> pressure_buffer       : register(u1);
 
-groupshared float shared_W[NUM_THREAD];
+groupshared float shared_W[256];
 groupshared uint  shared_nbr_sum[NUM_FP_CHUNK_MAX];
 
 [numthreads(NUM_THREAD,1,1)]
@@ -52,33 +52,42 @@ void main(uint3 Gid : SV_GroupID, uint Gindex : SV_GroupIndex)
   GroupMemoryBarrierWithGroupSync();
 
   // Thread별로 내가 어떤 FP_index에 어떤 nbr_index를 계산하는지 알아야 됨
-  uint FP_index   = 0;
-  uint nbr_index  = 0;
+  uint FP_index[N];
+  uint nbr_index[N];
   
   uint nbr_sum_prev = 0;
-  for (uint i=0; i<num_FP_chunk; ++i)
-  { 
-    FP_index = FP_index_begin +i;    
+  for (uint i=0; i<N; ++i)
+  {
+    const uint chunk_neighbor_index = N * Gindex + i;
+
+    for (uint j=0; j<num_FP_chunk; ++j)
+    { 
+      FP_index[i] = FP_index_begin + j;    
   
-    if (Gindex < shared_nbr_sum[i])
-    {
-      nbr_index = Gindex - nbr_sum_prev;    
-      break;
-    }      
+      if (chunk_neighbor_index < shared_nbr_sum[j])
+      {
+        nbr_index[i] = chunk_neighbor_index - nbr_sum_prev;    
+        break;
+      }      
   
-    nbr_sum_prev  = shared_nbr_sum[i];    
-  } 
+      nbr_sum_prev  = shared_nbr_sum[j];    
+    }   
+  }
+
   
   //계산
-  const uint ninfo_index  = FP_index * g_estimated_num_NFP + nbr_index;
-  const uint nbr_FP_index = ninfo_buffer[ninfo_index].fp_index;
-  
-  const float3  v_xi      = pos_buffer[FP_index];
-  const float3  v_xj      = pos_buffer[nbr_FP_index];
-  const float   distance  = length(v_xi-v_xj);
-  
-  shared_W[Gindex] = W(distance);  
-  
+  for (uint i=0; i<N; ++i)
+  {
+    const uint ninfo_index  = FP_index[i] * g_estimated_num_NFP + nbr_index[i];
+    const uint nbr_FP_index = ninfo_buffer[ninfo_index].fp_index;
+    
+    const float3  v_xi      = pos_buffer[FP_index[i]];
+    const float3  v_xj      = pos_buffer[nbr_FP_index];
+    const float   distance  = length(v_xi-v_xj);
+
+    shared_W[N * Gindex + i] = W(distance);
+  }
+    
   GroupMemoryBarrierWithGroupSync(); 
   
   if (Gindex < num_FP_chunk)
@@ -87,7 +96,7 @@ void main(uint3 Gid : SV_GroupID, uint Gindex : SV_GroupIndex)
     const uint nbr_index_end   = shared_nbr_sum[Gindex];
     
     float sum_W = 0.0;
-    for (uint i = nbr_index_begin; i < nbr_index_end; ++i)
+    for (uint i = nbr_index_begin; i < nbr_index_end; ++i) //여기가 잘못됐네
       sum_W += shared_W[i];
     
     const float rho      = sum_W * g_m0;
@@ -96,7 +105,7 @@ void main(uint3 Gid : SV_GroupID, uint Gindex : SV_GroupIndex)
     const uint  FP_index2 = FP_index_begin + Gindex;
     const float p         = pressure_buffer[FP_index2];
     
-    density_buffer[FP_index2]  = rho;// rho에 문제가 있나보다    
+    density_buffer[FP_index2]  = rho;
     pressure_buffer[FP_index2] = max(0.0, p + scailing_factor_buffer[0] * rho_err);  
   }
 }
